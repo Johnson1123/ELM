2e10c7614945423d43746cc24401385727b3eada
Johnson1123 <onifadejohnson2017@gmail.com>
2024-03-27 07:26:44 -0700

    retify cors policy

--- backend/app.ts
+++ backend/app.ts
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: origin,
     credentials: true,
   })
 );
@@ -30,11 +30,6 @@ app.use(
   layoutRoutes,
   orderRoutes
 );
-app.get("/get", (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "worked",
-  });
-});
 // const options = {
 //   definition: {
 //     openapi: "3.1.0",
